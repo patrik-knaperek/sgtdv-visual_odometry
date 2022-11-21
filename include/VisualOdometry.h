25d9a6d521bef0c38b89de6d017ab64b4ced2f35
Patrik Knaperek <pknaperek@gmail.com>
2022-11-21 20:12:39 +0100

    `visual_odometry` fixed: publishing base_link->odomTF; `fusion`: static transforms correction

--- include/VisualOdometry.h
+++ include/VisualOdometry.h
@@ -5,6 +5,7 @@
 
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 
@@ -14,9 +15,16 @@ class VisualOdometry {
         ~VisualOdometry();
 
         void BroadcastTf(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg);
-
+        void SetBaseFrameId(const std::string baseFrameId)
+        {
+            this->m_baseFrameId = baseFrameId;
+        };
 
     private:
+        
+        std::string m_baseFrameId;
+        tf::StampedTransform m_cameraToBaseTF;
         tf::TransformBroadcaster m_broadcaster;
+        tf::TransformListener m_listener;
 
 };
--- src/VisualOdometryNODE.cpp
+++ src/VisualOdometryNODE.cpp
@@ -7,10 +7,15 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "visual_odometry");
+    ros::init(argc, argv, "visualOdometry");
     ros::NodeHandle handle;
 
     VisualOdometry tfBroadcaster;
+
+    std::string baseFrame;
+    if(!handle.param("/base_frame", baseFrame, std::string("camera_center")))
+        ROS_INFO("Failed to get parameter \"/base_frame\" from server. Setting default value.\n");
+    tfBroadcaster.SetBaseFrameId(baseFrame);
 
     ros::Subscriber subscriber = handle.subscribe("/camera_pose", 1, &VisualOdometry::BroadcastTf, &tfBroadcaster);
 
